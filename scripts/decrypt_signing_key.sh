55a69f7363744099e21100b8b97905b1bb2de378
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-03-25 01:23:25 -0700

    Fix up signing.

--- scripts/decrypt_signing_key.sh
+++ scripts/decrypt_signing_key.sh
@@ -10,8 +10,8 @@ function cleanup {
 trap 'cleanup' ERR
 
 # Decrypt the file
-mkdir -p $HOME/secrets
+mkdir -p secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --passphrase="$SIGNING_KEY_PASSPHRASE" --output $HOME/secrets/methodic-keys.gpg-key --decrypt methodic-keys.gpg-key.gpg
-gpg --fast-import --no-tty --batch --yes $HOME/secrets/methodic-keys.gpg-key
+gpg --quiet --batch --yes --passphrase="$SIGNING_KEY_PASSPHRASE" --output ./secrets/methodic-keys.gpg-key --decrypt methodic-keys.gpg-key.gpg
+gpg --fast-import --no-tty --batch --yes ./secrets/methodic-keys.gpg-key
